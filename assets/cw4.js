36ce649211cabe55e7fc7e93b1f32b321264985c
Nour-Burezq <107755709+Nour-Burezq@users.noreply.github.com>
2022-07-01 21:10:03 +0300

    part 1 done

---
+++ assets/cw4.js
@@ -0,0 +1,7 @@
+alert( "مرحباً بك في موقعي! انا نور، وسوف احسب درجتك")
+        let grade = prompt("ادخل درجتك");
+        
+        console.log("%c your grade is ","color:red");
+        console.log(grade);
+
+        
